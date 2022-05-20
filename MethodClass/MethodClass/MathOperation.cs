f2da050968479eb40043b3f6e2203dcb9c7f404f
kimten88 <kimtendulkar88@gmail.com>
2022-05-20 12:25:36 -0700

    Method Class Update

--- MethodClass/MethodClass/MathOperation.cs
+++ MethodClass/MethodClass/MathOperation.cs
@@ -9,12 +9,10 @@ namespace MethodClass
         //created void method with two numbers
         public void OperationMethod (int value1, int value2)
         {
-            //assigning the two numbers
-            value1 = 5;
-            value2 = 7;
+
             //method doing the math operation with first integer
             int total = value1 * 2;
-            Console.WriteLine("Value 1 * 2 = " + total);
+            Console.WriteLine(value1 + " * 2 = " + total);
             //displaying second integer
             Console.WriteLine("Value 2 = " + value2);
         }
--- MethodClass/MethodClass/Program.cs
+++ MethodClass/MethodClass/Program.cs
@@ -6,19 +6,16 @@ namespace MethodClass
     {
         static void Main(string[] args)
         {
-            //gave the varaibles to use on program.cs
-            int value1 = 5;
-            int value2 = 7;
+
             //instantiated the class
             MathOperation m = new MathOperation();
             
             //calling the method and passing in the two numbers
-            m.OperationMethod(value1, value2);
+            m.OperationMethod(5, 7);
 
             //specifying the parameters by name
-            m.OperationMethod(value1, value2);
-            Console.WriteLine("Value1 is equal to: " + value1);
-            Console.WriteLine("Value2 is qual to: " + value2);
+            m.OperationMethod(value1: 3, value2: 20);
+            
         }
     }
 }
