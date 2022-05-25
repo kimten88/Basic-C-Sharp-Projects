f47a41a18fc683ac455292cd4b7dca4df6646846
kimten88 <kimtendulkar88@gmail.com>
2022-05-24 20:29:37 -0700

    Polymorphism

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,11 +10,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Kim", "Abhijit", "Nimbus", "Raja" };
-            game.ListPlayers();
-            Console.ReadLine();
-
             Deck deck = new Deck();
             deck.Shuffle(3);
 
