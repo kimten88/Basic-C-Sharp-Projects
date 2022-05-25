9d3b08e488d5f1bcdbdd7abc2d457f12c8003aef
kimten88 <kimtendulkar88@gmail.com>
2022-05-24 17:24:22 -0700

    Class Submission Assignment

---
+++ Class Submission/Class Submission/ClassClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Submission
+{
+    static class ClassClass
+    {
+
+        public static void Divide(int userValue, out int total)
+        {
+
+            //establish total as userValue divided by 2
+            total = userValue / 2;
+            
+
+        }
+        //overload this method
+        public static int Multiply(int userValue)
+        {
+            int result = userValue * 10;
+            return result;
+
+        }
+        public static int Multiply(int userValue, int userValue2)
+        {
+            int result = userValue * userValue2 *10;
+            return result;
+
+        }
+
+    }
+}
---
+++ Class Submission/Class Submission/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Class_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //establishing variable for use
+            int total;
+            //asks user for a number
+            Console.WriteLine("Please indicate a number: ");
+            //tells the program to use the user's input as an integer
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            //calling method with output 
+            ClassClass.Divide(userValue, out total);
+            //displaying to the user
+            Console.WriteLine(userValue + " / 2  = " + total);
+            Console.ReadLine();
+            //asking for optional number to create opportunity for overload
+            Console.WriteLine("Please select another number - optional: ");
+            int userValue2;
+            //telling the program which overload method to use
+            if (int.TryParse(Console.ReadLine(), out userValue2))
+            {
+                //calling Multiply method from ClassClass
+                int MultiTotal = ClassClass.Multiply(userValue, userValue2);
+                //displays results to the user
+                Console.WriteLine(userValue + " * " + userValue2 + " * 10 = " + MultiTotal);
+
+            }
+            else
+            {
+                //calling Multiply method from ClassClass.cs
+                int MultiTotal = ClassClass.Multiply(userValue);
+                //displays results to the user
+                Console.WriteLine(userValue + " * 10 = " + MultiTotal);
+            }
+
+        }
+    }
+}
