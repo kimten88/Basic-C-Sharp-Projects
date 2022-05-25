a85adec32eb83482aae2ae7f9ad800699f166fdd
kimten88 <kimtendulkar88@gmail.com>
2022-05-24 17:25:23 -0700

    Update ClassClass.cs

--- Class Submission/Class Submission/ClassClass.cs
+++ Class Submission/Class Submission/ClassClass.cs
@@ -4,27 +4,28 @@ using System.Text;
 
 namespace Class_Submission
 {
+    //static class
     static class ClassClass
     {
-
+        //static void method which outputs an integer
         public static void Divide(int userValue, out int total)
         {
 
             //establish total as userValue divided by 2
             total = userValue / 2;
-            
 
         }
-        //overload this method
+        //overload this method with output parameters
         public static int Multiply(int userValue)
         {
             int result = userValue * 10;
             return result;
 
         }
+        //overloaded method, option if user enters 2 values
         public static int Multiply(int userValue, int userValue2)
         {
-            int result = userValue * userValue2 *10;
+            int result = userValue * userValue2 * 10;
             return result;
 
         }
