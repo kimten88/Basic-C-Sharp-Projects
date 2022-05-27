e8d342e013a2226758ad8da123b01d1e9da1e16a
kimten88 <kimtendulkar88@gmail.com>
2022-05-27 11:00:21 -0700

    update

--- ParsingEnums/ParsingEnums/Program.cs
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -4,6 +4,7 @@ namespace ParsingEnums
 {
     class Program
     {
+        //enum
         public enum DaysOfTheWeek
         {
             Monday,
@@ -20,12 +21,17 @@ namespace ParsingEnums
 
             try
             {
+                //asking user to enter day
                 Console.WriteLine("What day is it?");
+                //assigning variable to user input
                 string userAnswer = Console.ReadLine();
+                //assigning the value to a variable of that enum data type 
                 DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userAnswer);
+                //displaying the enum data to the user
                 Console.WriteLine(day + " is today!");
 
             }
+            //if the user does not enter in information from the enum
             catch (Exception)
             {
                 Console.WriteLine("Please enter an acutal day of the week");
