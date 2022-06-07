ff9011bf10fa3f7bd0ce362c3f3139a697b256a5
kimten88 <kimtendulkar88@gmail.com>
2022-06-07 13:14:02 -0700

    CodeFirst Submission

---
+++ CodeFirst/CodeFirst/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirst
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var ctx = new SchoolContext();
+            var stud = new Student() { StudentName = "Raja" 
+        };
+
+            ctx.Students.Add(stud);
+            ctx.SaveChanges();
+        }
+    }
+}
